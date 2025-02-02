f2894e71d88798503a2c2822956d8adf8721ee9c
Devin <threateninglyamusing@gmail.com>
2025-02-02 02:20:00 -0800

    -

--- server.py
+++ server.py
@@ -2,8 +2,6 @@ import os
 from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
 
-# Set the directory where your files are located (use the current directory by default)
-
 # Set the port for the server (default is 8000)
 PORT = 8000
         
