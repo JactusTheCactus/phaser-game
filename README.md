0503bb1f3602fe900ac30da906c8f306679cbe5b
Devin <threateninglyamusing@gmail.com>
2025-02-02 02:06:11 -0800

    -

---
+++ README.md
@@ -0,0 +1,1 @@
+# __[Link](http://localhost:8000)__
--- server.py
+++ server.py
@@ -7,7 +7,13 @@ from socketserver import TCPServer
 # Set the port for the server (default is 8000)
 PORT = 8000
 
+def readme(port):
+    markdown = f"""# __[Link](http://localhost:{PORT})__"""
+    with open('README.md','w') as file:
+        file.write(markdown)
+
 # Create a TCP server with the SimpleHTTPRequestHandler to serve files
 with TCPServer(("", PORT), SimpleHTTPRequestHandler) as httpd:
     print(f"Serving files from {os.getcwd()} at http://localhost:{PORT}")
+    readme(PORT)
     httpd.serve_forever()
